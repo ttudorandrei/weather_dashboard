a9b468b989037e0caae7cc0f492f3211e0b9e17e
Tudor Tocan <tudor.tocan@gmail.com>
2021-04-11 13:20:13 +0100

    add function to push user cities in array and array in local storage

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,7 +1,27 @@
+//this function gets the city names from local storage
+const getFromLocalStorage = () => {
+  const localStorageData = JSON.parse(localStorage.getItem("cities"));
+
+  if (localStorageData === null) {
+    return [];
+  } else {
+    return localStorageData;
+  }
+};
+
+//this function retrieves data from input box
 const onSubmit = (event) => {
   event.preventDefault();
+
   const cityName = $("#city-input").val();
-  console.log(cityName);
+  const cities = getFromLocalStorage();
+
+  //this pushes user city input into array
+  cities.push(cityName);
+
+  //this saves array into local storage
+  localStorage.setItem("cities", JSON.stringify(cities));
+  console.log(cities);
 };
 
 $("#search-by-city-form").on("submit", onSubmit);
