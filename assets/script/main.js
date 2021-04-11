c226fb4ab205f9b25ec464b6b410329a46cbefe1
Tudor Tocan <tudor.tocan@gmail.com>
2021-04-11 13:38:12 +0100

    add function to append city search history from local storage to list

--- assets/script/main.js
+++ assets/script/main.js
@@ -24,4 +24,28 @@ const onSubmit = (event) => {
   console.log(cities);
 };
 
+const renderCitiesFromLocalStorage = () => {
+  //this gets cities from local storage
+  const cities = getFromLocalStorage();
+
+  //creates ul
+  const ul = $("<ul>").addClass("list-group");
+
+  //for each city in local storage create li and append to target ul
+  const appendListItemToUl = (city) => {
+    console.log(city);
+    const li = `<li class="list-group-item">${city}</li>`;
+    ul.append(li);
+  };
+
+  cities.forEach(appendListItemToUl);
+
+  $("#searched-cities").append(ul);
+};
+
+const onReady = () => {
+  renderCitiesFromLocalStorage();
+};
+
 $("#search-by-city-form").on("submit", onSubmit);
+$(document).ready(onReady);
